a6986f7f518957477112bb96086ea14fa9025a75
Niklas Koskela <niklas.koskela@aalto.fi>
2023-09-27 11:10:46 +0300

    2.11

--- part2/puhelinluettelo/src/App.jsx
+++ part2/puhelinluettelo/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const FilterForm = (props) => {
   return(
@@ -8,6 +9,7 @@ const FilterForm = (props) => {
   </div>
   )
 }
+
 
 const PersonForm = (props) => {
 
@@ -37,14 +39,19 @@ const Persons = (props) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
 
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
 
+  console.log('render', persons.length, 'persons')
 
   const [filterWord, setFilterWord] = useState('')
   const handleFilterChange = (event) => {
