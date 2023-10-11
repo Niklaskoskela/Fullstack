ea4162eb861bbcf8b78472a1441b9ef8c60e5eed
Niklas Koskela <niklas.koskela@aalto.fi>
2023-10-11 12:16:08 +0300

    4.2

--- part4/app.js
+++ part4/app.js
@@ -1,17 +1,26 @@
+// node modules
 const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+
+// own modules 
 const config = require('./utils/config')
 const logger = require('./utils/logger')
-
+const middleware = require('./utils/middleware')
 
 const blogsRouter = require('./controllers/blogs')
+
+//PRE app
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
+app.use(middleware.requestLogger)
+
 app.use('/api/blogs', blogsRouter)
 
-app.use(cors())
-app.use(express.json())
-
-const PORT = config.PORT
+//AFTER app
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
