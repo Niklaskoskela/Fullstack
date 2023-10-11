51962eda67912ecabcda6ef1d61063360ea2f02f
Niklas Koskela <niklas.koskela@aalto.fi>
2023-10-11 14:51:50 +0300

    4.7

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash')
+
 const dummy = (blogs) => {
     return 1
   }
@@ -29,6 +31,72 @@ const favouriteBlog = (blogs) => {
   return favourite
 }
 
+const mostBlogs = (blogs) => {
+  //null check
+  if (blogs.length === 0){
+    return null
+  }
+  // grouped: Map(writer, blogs)
+
+  const grouped = _.groupBy(blogs, blog => blog.author)
+  
+  var mostBlogs = null
+  let mostBlogsAmount = 0
+
+  _.forOwn(grouped, (blogs,author) => {
+    if (mostBlogs === null | blogs.length > mostBlogsAmount){
+      //JSON
+
+      const blogger = {
+        "author": author,
+        "blogs": blogs.length
+      }
+      mostBlogsAmount = blogs.length
+      mostBlogs = blogger
+    }
+  }) 
+
+  return mostBlogs
+}
+
+
+const mostLikes = (blogs) => {
+  //null check
+  if (blogs.length === 0){
+    return null
+  }
+
+  // grouped: Map(writer, blogs)
+  const grouped = _.groupBy(blogs, blog => blog.author)
+  //keep track
+  let mostLikes = null
+  let mostLikesAmount = 0
+
+  //iterate over grouped
+  _.forOwn(grouped, (blogs,author) => {
+    
+    let likes = 0
+    //calc likes
+    blogs.forEach( blog => {
+      likes += blog.likes
+    })
+    //update if necessary
+    if (mostLikes === null | likes > mostLikesAmount){
+      //JSON
+
+      const blogger = {
+        "author": author,
+        "likes": likes
+      }
+      mostLikesAmount = likes
+      mostLikes = blogger
+    }
+  }) 
+
+  return mostLikes
+}
+
+
 module.exports = {
-  dummy, totalLikes, favouriteBlog
+  dummy, totalLikes, favouriteBlog, mostBlogs, mostLikes
 }
