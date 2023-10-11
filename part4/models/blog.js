df5f8771c784cf62848a15229764653093ea2290
Niklas Koskela <niklas.koskela@aalto.fi>
2023-10-11 12:01:42 +0300

    app.js functional

--- part4/models/blog.js
+++ part4/models/blog.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose')
+const logger = require('./../utils/logger')
+const config = require('./../utils/config')
 require('dotenv').config();
+
 
 
 const blogSchema = mongoose.Schema({
@@ -11,9 +14,9 @@ const blogSchema = mongoose.Schema({
 
 
 
-const mongoUrl = process.env.MONGODB_URI
+const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl).then( () =>
-    console.log("connected to MONGODB")
+    logger.info("connected to MONGODB")
 )
 
 module.exports = mongoose.model('Blog', blogSchema)
